e3641eca8c3554bcd04652248df3300f984bdd39
hanna <hanna-881@hotmail.com>
2023-10-07 16:54:51 -0400

    commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  api_url: 'https://q-a-example-loopback-api.herokuapp.com'
+  api_url: 'https://j2i1fztzp5.execute-api.us-east-1.amazonaws.com'
 };
