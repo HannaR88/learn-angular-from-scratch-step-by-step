9c960e9d123248837a6c1cd1b18298ab5c592132
hanna <hanna-881@hotmail.com>
2023-10-07 20:57:01 -0400

    commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  api_url: 'https://j2i1fztzp5.execute-api.us-east-1.amazonaws.com'
+  api_url: 'https://xjiark33z6.execute-api.us-east-1.amazonaws.com'
 };
